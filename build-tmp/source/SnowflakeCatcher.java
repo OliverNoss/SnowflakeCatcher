9064f6fb3c0d03d32f7d410b1bf37e7b9ed473ac
OliverNoss <odnoss@sbcglobal.net>
2014-10-03 12:19:01 -0700

    submitting snowflake catcher for first time

--- build-tmp/source/SnowflakeCatcher.java
+++ build-tmp/source/SnowflakeCatcher.java
@@ -1,7 +1,22 @@
-// Oliver Noss AP Computer Science
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class SnowflakeCatcher extends PApplet {
+
 SnowFlake[] snow = new SnowFlake[1000];
 
-void setup()
+public void setup()
 {
   size(500,500);
   background(0);
@@ -12,7 +27,7 @@ void setup()
   }
   background(0);
 }
-void draw()
+public void draw()
 {
   fill(0);
   stroke(0);
@@ -35,7 +50,7 @@ void draw()
     }  
   }
 }
-void mouseDragged()
+public void mouseDragged()
 {if(mouseButton==LEFT)
   {
   stroke(0,150,255);
@@ -46,7 +61,7 @@ else if (mouseButton==RIGHT) {
   strokeWeight(4);
   line(pmouseX,pmouseY,mouseX,mouseY);
 }
-void keyPressed()
+public void keyPressed()
 {
   if (key=='q')
     {
@@ -64,18 +79,18 @@ class SnowFlake
     x=(int)(Math.random()*500);
     y=0;
     d=(int)(Math.random()*4+2);
-    dy=(float)(Math.random()*.8+.3);
+    dy=(float)(Math.random()*.8f+.3f);
     isMoving = true;
   }
-  void show()
+  public void show()
   {
     noStroke();
     fill(255);
     ellipse(x,y,d,d);
   }
-  void lookDown()
+  public void lookDown()
   {
-    if(get(x,int(y+d/2+3))!=color(0,0,0))
+    if(get(x,PApplet.parseInt(y+d/2+3))!=color(0,0,0))
       {
         isMoving=false;
       }
@@ -84,19 +99,28 @@ class SnowFlake
         isMoving=true;        
       }
   }
-  void erase()
+  public void erase()
   {
     stroke(0);
     fill(0);
     ellipse(x,y-2,d+2,d+1);
   }
-  void move()
+  public void move()
   {
     if (isMoving)
     y+=dy;
   }
-  boolean wrap()
+  public boolean wrap()
   {
     return y>496-d/2;  
   }
 }//
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "SnowflakeCatcher" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
